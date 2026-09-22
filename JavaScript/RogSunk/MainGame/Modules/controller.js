d3c167030f4da624f5be399c8f80ea54bcaf9c3d
Roger Williams <93908183+RogerWilliams2026@users.noreply.github.com>
2026-09-22 11:38:11 +0100

    Add files via upload

---
+++ JavaScript/RogSunk/MainGame/Modules/controller.js
@@ -0,0 +1,12 @@
+"use strict";
+/*
+
+  games main loop centres around document event handler
+  for key press
+
+  there is a timer that runs to move the clouds and another to move the sub, the fish, the time limit, and the depth charge!
+
+*/
+import { funcInit } from "./model.js";
+//setup and start game
+funcInit();
